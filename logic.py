500e9cd39d385d9f5607ef16bcc774f20c1adf67
aktec <aktec@mail.ru>
2024-08-31 15:01:37 +0300

    add def process_image

--- logic.py
+++ logic.py
@@ -1,18 +1,18 @@
 import cv2
 
-
 def process_image(image_path, output_path):
     # Загрузка предобученного классификатора для детектирования лиц
+    print(cv2.data.haarcascades)
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
     # Загрузка изображения
-    image = 
+    image = cv2.imread(image_path)
 
     # Преобразование изображения в оттенки серого для улучшения производительности детектора
-    gray = 
+    gray =  cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Обнаружение лиц в изображении
-    faces = 
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
     # Размытие области вокруг каждого обнаруженного лица
     for (x, y, w, h) in faces:
@@ -20,13 +20,13 @@ def process_image(image_path, output_path):
         face_region = image[y:y+h, x:x+w]
 
         # Применение размытия
-        blurred_face = 
+        blurred_face = cv2.GaussianBlur(face_region, (99, 99), 30)
 
         # Замена области лица размытым изображением
         image[y:y+h, x:x+w] = blurred_face
 
     # Сохранение обработанного изображения
-    
+    cv2.imwrite(output_path, image)
 
 
 # Проверяем работу кода
